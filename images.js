b970a97371bb661cc04c529c13619f2fceb2b8bd
rmarjora <rasmus.marjoranta@gmail.com>
2025-06-10 13:26:21 +0300

    added images and buttons

---
+++ images.js
@@ -0,0 +1,6 @@
+const prev = document.querySelector("#previous-arrow")
+const next = document.querySelector("#next-arrow")
+const mainImg = document.querySelectorAll(".main-image")
+
+const viewImages = false
+const imageIndex = 0
